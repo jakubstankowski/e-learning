1ef0cffabb7476e8e474e584550604e553b5ece2
jakub <jakubstankowski@protonmail.com>
2020-12-20 12:12:02 +0100

    EL-1 connect repo to kanban board

--- E-Learning.Domain/Model/Courses.cs
+++ E-Learning.Domain/Model/Courses.cs
@@ -6,5 +6,11 @@ namespace E_Learning.Domain.Model
 {
     class Courses
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
     }
 }
