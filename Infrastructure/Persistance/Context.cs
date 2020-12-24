2b87d6479491e7193333a53bff0bfeec1927923d
jakub <jakubstankowski@protonmail.com>
2020-12-24 12:35:59 +0100

    EL-3 add database context with application user

---
+++ Infrastructure/Persistance/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Learning.Domain.Model;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistance
+{
+    public class Context : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Courses> Courses { get; set; }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+
+    }
+}
