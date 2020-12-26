ccaa589005345f9f63d24ace736e1cecddc371ee
jakub <jakubstankowski@protonmail.com>
2020-12-26 18:49:21 +0100

    LM-3 add courses services interface

---
+++ Application/Common/Interfaces/ICoursesServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Learning.Domain.Model;
+
+namespace E_Learning.Application.Common.Interfaces
+{
+   public  interface ICoursesServices
+    {
+        public IEnumerable<Courses> GetAllCourses();
+
+        public void AddNewCourses();
+    }
+}
