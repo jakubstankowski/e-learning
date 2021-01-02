e7b1ee5037fd20e1578c104faf2052dc3f69681f
jakub <jakubstankowski@protonmail.com>
2021-01-02 15:51:40 +0100

    Change main projects connections

---
+++ Infrastructure/DepedencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Learning.Application.Common.Interfaces;
+using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<ICoursesServices, CoursesServices>();
+
+            return services;
+        }
+    }
+}
