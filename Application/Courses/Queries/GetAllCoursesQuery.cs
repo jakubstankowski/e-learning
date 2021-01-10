d0652b1ca7baeba685f701852dcc80c766778deb
jakub <jakubstankowski@protonmail.com>
2021-01-10 12:31:00 +0100

    EL-2  put whole logic from handle to get all courses query

--- Application/Courses/Queries/GetAllCoursesQuery.cs
+++ Application/Courses/Queries/GetAllCoursesQuery.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using E_Learning.Application.Common.Dto;
+using E_Learning.Application.Common.Interfaces;
+using E_Learning.Domain.Entities;
 using MediatR;
 
 namespace E_Learning.Application.Courses1.Queries
 {
     public class GetAllCoursesQuery : IRequest<IEnumerable<CourseDto>>
     {
+    }
+
+    public class GetAllCoursesHandler : IRequestHandler<GetAllCoursesQuery, IEnumerable<CourseDto>>
+    {
+        private readonly ICoursesServices _coursesServices;
+        private readonly IMapper _mapper;
+
+        public GetAllCoursesHandler(ICoursesServices coursesServices, IMapper mapper)
+        {
+            _coursesServices = coursesServices;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CourseDto>> Handle(GetAllCoursesQuery request, CancellationToken cancellationToken)
+        {
+            var courses = await _coursesServices.GetAllCourses();
+            return _mapper.Map<IEnumerable<Course>, IEnumerable<CourseDto>>(courses);
+        }
     }
 }
