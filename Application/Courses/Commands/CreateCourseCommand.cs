e24023567d44e7b164ad1f6d032a9ba6e5fa6ad2
jakub <jakubstankowski@protonmail.com>
2021-01-10 12:27:23 +0100

    EL-2 remove handler and put whole logic to create course command

--- Application/Courses/Commands/CreateCourseCommand.cs
+++ Application/Courses/Commands/CreateCourseCommand.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using E_Learning.Application.Common.Dto;
+using E_Learning.Application.Common.Interfaces;
 using E_Learning.Domain.Entities;
 using MediatR;
 
@@ -19,7 +18,38 @@ namespace E_Learning.Application.Courses.Commands
 
         public string VideoUrl { get; set; }
 
-
-
     }
+
+
+    public class CreateCourseHandler : IRequestHandler<CreateCourseCommand, CourseDto>
+    {
+        private readonly ICoursesServices _coursesServices;
+        private readonly IMapper _mapper;
+
+        public CreateCourseHandler(ICoursesServices coursesServices, IMapper mapper)
+        {
+            _coursesServices = coursesServices;
+            _mapper = mapper;
+        }
+
+        public async Task<CourseDto> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Description = request.Description,
+                VideoUrl = request.VideoUrl
+            };
+
+            course.Title = request.Title;
+            course.VideoUrl = request.VideoUrl;
+            course.Description = request.Description;
+
+            await _coursesServices.AddNewCourses(course);
+
+            return _mapper.Map<Course, CourseDto>(course);
+        }
+    }
+
 }
