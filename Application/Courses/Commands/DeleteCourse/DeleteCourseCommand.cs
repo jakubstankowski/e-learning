a092b3d36be4a064b895ea340607d008ea7c700f
jakub <jakubstankowski@protonmail.com>
2021-01-10 14:06:46 +0100

    EL-6 add delete course command, and change basic CQRS project architecture

---
+++ Application/Courses/Commands/DeleteCourse/DeleteCourseCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using E_Learning.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Learning.Application.Courses.Commands.DeleteCourse
+{
+    public class DeleteCourseCommand : IRequest
+    {
+        public int Id { get; set; }
+
+    }
+
+
+    public class DeleteCourseHandler : IRequestHandler<DeleteCourseCommand>
+    {
+        private readonly IContext _context;
+
+        public DeleteCourseHandler(IContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == request.Id);
+
+            if (course == null)
+            {
+                throw new NotFoundException(nameof(TodoList), request.Id);
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+
+
+            return Unit.Value;
+        }
+    }
+}
