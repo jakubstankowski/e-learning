fd92b64c705d40707d31dec40484a878e406d507
jakub <jakubstankowski@protonmail.com>
2021-01-10 12:38:37 +0100

    EL-2 put whole logic from handler to get course by id query

--- Application/Courses/Queries/GetCoursesByIdQuery.cs
+++ Application/Courses/Queries/GetCoursesByIdQuery.cs
@@ -1,4 +1,9 @@
-﻿using E_Learning.Application.Common.Dto;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using E_Learning.Application.Common.Dto;
+using E_Learning.Application.Common.Interfaces;
+using E_Learning.Domain.Entities;
 using MediatR;
 
 namespace E_Learning.Application.Courses.Queries
@@ -10,6 +15,32 @@ namespace E_Learning.Application.Courses.Queries
         public GetCoursesByIdQuery(int id)
         {
             this.Id = id;
+        }
+
+
+        public class GetCourseByIdHandler : IRequestHandler<GetCoursesByIdQuery, CourseDto>
+        {
+            private readonly ICoursesServices _coursesServices;
+
+            public IMapper _mapper { get; }
+
+            public GetCourseByIdHandler(ICoursesServices services, IMapper mapper)
+            {
+                _coursesServices = services;
+                _mapper = mapper;
+            }
+
+            public async Task<CourseDto> Handle(GetCoursesByIdQuery request, CancellationToken cancellationToken)
+            {
+                var course = await _coursesServices.GetCourseById(request.Id);
+
+                if (course == null)
+                {
+                    return null;
+                }
+
+                return _mapper.Map<Course, CourseDto>(course);
+            }
         }
 
     }
