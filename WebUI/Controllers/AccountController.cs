d194a9c2d97b3d5fe6462f0705a8d9407c062b39
jakub <jakubstankowski@protonmail.com>
2021-01-16 15:56:17 +0100

    EL-3 add basic account controller

---
+++ WebUI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public ActionResult Register()
+        {
+
+        }
+    }
+
+
+}
