035f3188afa6d2493f30bcf2347e8a512ef961db
jakub <jakubstankowski@protonmail.com>
2021-01-18 18:13:09 +0100

    EL-3 add claims while generate jwt token

--- Infrastructure/Identity/IdentityService.cs
+++ Infrastructure/Identity/IdentityService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using E_Learning.Application.Common.Interfaces;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
@@ -19,20 +21,26 @@ namespace Infrastructure.Identity
         {
             _config = config;
         }
-      
+
         public string GenerateToken(ApplicationUser user)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(
-                _config["Jwt:Issuer"], 
-                _config["Jwt:Issuer"],
-                null,
-                expires: DateTime.Now.AddMinutes(120), 
-                signingCredentials: credentials);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(3),
+                Subject = new ClaimsIdentity(new[]
+           {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email)
+            }),
+                SigningCredentials = new SigningCredentials(key: securityKey, algorithm: SecurityAlgorithms.HmacSha256Signature)
+            };
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            var Securitytoken = new JwtSecurityTokenHandler().CreateToken(tokenDescriptor);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(Securitytoken);
         }
     }
 }
