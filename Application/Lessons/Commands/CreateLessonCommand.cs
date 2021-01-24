e0cbc20ab026f9c848e665c85055e6dde0039f6f
jakub <jakubstankowski@protonmail.com>
2021-01-24 18:47:36 +0100

    EL-9 add course id to Create lesson command, add lesson controller

--- Application/Lessons/Commands/CreateLessonCommand.cs
+++ Application/Lessons/Commands/CreateLessonCommand.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using E_Learning.Application.Common.Interfaces;
 using E_Learning.Application.Lessons.Queries.GetLessons;
 using MediatR;
 
 namespace E_Learning.Application.Lessons.Commands
 {
-   public class CreateLessonCommand : IRequest<LessonDto>
+    public class CreateLessonCommand : IRequest<LessonDto>
     {
         public string Title { get; set; }
 
@@ -16,5 +16,25 @@ namespace E_Learning.Application.Lessons.Commands
 
         public string VideoUrl { get; set; }
 
+        public int CourseId { get; set; }
+
     }
+
+    public class CreateLessonHandler : IRequestHandler<CreateLessonCommand, LessonDto>
+    {
+        public CreateLessonHandler(IContext context, IMapper mapper)
+        {
+
+        }
+
+        public Task<LessonDto> Handle(CreateLessonCommand request, CancellationToken cancellationToken)
+        {
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+
+
 }
---
+++ WebUI/Controllers/LessonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LessonController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
