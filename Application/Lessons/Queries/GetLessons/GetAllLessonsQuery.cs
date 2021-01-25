dd41c78f00e9b5fc13f2fd8fcecac3b6bea495d0
jakub <jakubstankowski@protonmail.com>
2021-01-25 17:26:14 +0100

    EL-13 add get all lessons query with handler

---
+++ Application/Lessons/Queries/GetLessons/GetAllLessonsQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using E_Learning.Application.Common.Interfaces;
+using MediatR;
+
+namespace E_Learning.Application.Lessons.Queries.GetLessons
+{
+    public class GetAllLessonsQuery : IRequest<IEnumerable<LessonDto>>
+    {
+      
+    }
+
+    public class GetAllLessonsHandler : IRequestHandler<GetAllLessonsQuery, IEnumerable<LessonDto>>
+    {
+        public Task<IEnumerable<LessonDto>> Handle(GetAllLessonsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
