afff3eef01159ae8009523d31728324bd1aa5673
jakub <jakubstankowski@protonmail.com>
2021-01-26 19:45:50 +0100

    EL-13 finish implement update lesson

--- Application/Lessons/Queries/GetLessons/LessonDto.cs
+++ Application/Lessons/Queries/GetLessons/LessonDto.cs
@@ -8,6 +8,7 @@ namespace E_Learning.Application.Lessons.Queries.GetLessons
 {
     public class LessonDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
 
         public string Description { get; set; }
