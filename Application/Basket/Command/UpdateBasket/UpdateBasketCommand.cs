4d1b51aa2c9293b6a960c17ff470f6de33603969
jakub <jakubstankowski@protonmail.com>
2021-01-27 20:08:07 +0100

    EL-14 add customer basket to update basket command

--- Application/Basket/Command/UpdateBasket/UpdateBasketCommand.cs
+++ Application/Basket/Command/UpdateBasket/UpdateBasketCommand.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using E_Learning.Domain.Entities;
+using MediatR;
+using StackExchange.Redis;
 
 namespace E_Learning.Application.Basket.Command.UpdateBasket
 {
-   public class UpdateBasketCommand
+    public class UpdateBasketCommand : IRequest<CustomerBasket>
     {
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
     }
+
+    public class UpdateBasketHandler : IRequestHandler<UpdateBasketCommand, CustomerBasket>
+    {
+        private readonly IDatabase _database;
+
+        public UpdateBasketHandler(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        public Task<CustomerBasket> Handle(UpdateBasketCommand request, CancellationToken cancellationToken)
+        {
+            var customerBasket = new CustomerBasket
+            {
+                Id = request.Id,
+                Items = request.Items
+            };
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+
 }
