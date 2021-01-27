50f925aca3e005a0426b0aabb353d649ed67edc1
jakub <jakubstankowski@protonmail.com>
2021-01-27 20:12:08 +0100

    EL-14 implement basic basket controller

---
+++ WebUI/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_Learning.Application.Basket.Command.UpdateBasket;
+using E_Learning.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BasketController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> Create(UpdateBasketCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
