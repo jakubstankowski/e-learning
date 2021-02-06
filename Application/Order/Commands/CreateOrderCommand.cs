a02d33a9ac14b5e6e80214665773422fe8f21e2d
jakub <jakubstankowski@protonmail.com>
2021-02-06 16:39:37 +0100

    EL-15 add order controller with create order method

--- Application/Order/Commands/CreateOrderCommand.cs
+++ Application/Order/Commands/CreateOrderCommand.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using E_Learning.Application.Basket.Queries.GetBasket;
+using MediatR;
 
 namespace E_Learning.Application.Order.Commands
 {
-    class CreateOrderCommand
+    public class CreateOrderCommand : IRequest<bool>
     {
+        public string BasketId { get; set; }
+
     }
+
+    public class CreateOrderHandler : IRequestHandler<CreateOrderCommand, bool>
+    {
+        public Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var basket = new GetBasketByIdQuery(request.BasketId);
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+
 }
---
+++ WebUI/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_Learning.Application.Order.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
