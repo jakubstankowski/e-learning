64c85aa2fff13179df1c5b2ea5b51db6a2caa39d
jakub <jakubstankowski@protonmail.com>
2021-02-07 12:38:08 +0100

    EL-15 get correct course item from database in create order command

--- Application/Orders/Commands/CreateOrderCommand.cs
+++ Application/Orders/Commands/CreateOrderCommand.cs
@@ -5,7 +5,12 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using E_Learning.Application.Basket.Queries.GetBasket;
+using E_Learning.Application.Common.Exceptions;
+using E_Learning.Application.Common.Interfaces;
+using E_Learning.Domain.Entities;
+using E_Learning.Domain.Entities.OrderAggregate;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Learning.Application.Orders.Commands
 {
@@ -18,19 +23,35 @@ namespace E_Learning.Application.Orders.Commands
     public class CreateOrderHandler : IRequestHandler<CreateOrderCommand, bool>
     {
         private readonly IMediator _mediator;
+        private readonly IContext _context;
 
-        public CreateOrderHandler(IMediator mediator)
+        public CreateOrderHandler(IMediator mediator, IContext context)
         {
             _mediator = mediator;
+            _context = context;
         }
 
         public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
             var query = new GetBasketByIdQuery(request.BasketId);
-            var result = await _mediator.Send(query);
+            var basket = await _mediator.Send(query);
 
 
 
+
+            var items = new List<OrderItem>();
+            foreach (var item in basket.Items)
+            {
+                var courseItem = await _context.Courses.FirstOrDefaultAsync(c => c.Id == item.Id);
+
+                if (courseItem == null)
+                {
+                    throw new NotFoundException(nameof(Course), item.Id);
+                }
+
+
+             
+            }
 
             throw new NotImplementedException();
         }
