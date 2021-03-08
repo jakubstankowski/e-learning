d4d93ec0615eaff08cbdf9edfbbaf937a87a0aa7
jakub <jakubstankowski@protonmail.com>
2021-03-08 16:23:40 +0100

    EL-21 fixed bug with header location

--- WebUI/ClientApp/e-learning-ui/src/App.js
+++ WebUI/ClientApp/e-learning-ui/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import Header from "../components/layout/Header";
+import Header from "./components/layout/Header";
+import React from "react";
 
 
 function App() {
---
+++ WebUI/ClientApp/e-learning-ui/src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h3>Last Minute</h3>
+            </header>
+        )
+    }
+}
+
+export default Header;
