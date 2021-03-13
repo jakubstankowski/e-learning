bdf15c212e06f1dd6e3638e8efde1cb675368bab
jakub <jakubstankowski@protonmail.com>
2021-03-13 13:29:12 +0100

    Remove unused components from Course.js

--- WebUI/ClientApp/e-learning-ui/src/components/courses/Course.js
+++ WebUI/ClientApp/e-learning-ui/src/components/courses/Course.js
@@ -2,8 +2,6 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Grid from "@material-ui/core/Grid";
 
