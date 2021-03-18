85b43a99c6a9003248860803f873a3fcf9a3dcf3
jakub <jakubstankowski@protonmail.com>
2021-03-18 16:43:32 +0100

    Rename function to class in App.js

--- WebUI/ClientApp/e-learning-ui/src/App.js
+++ WebUI/ClientApp/e-learning-ui/src/App.js
@@ -7,19 +7,23 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Course from "./components/courses/Course";
 
 
-function App() {
-    return (
-        <Router>
-            <div>
-                <Header/>
-                <Container>
-                    <Route path="/" exact component={Courses}/>
-                    <Route path="/course/:id" exact component={Course}/>
-                </Container>
-            </div>
-        </Router>
+class App extends React.Component {
 
-    );
+
+    render(){
+        return (
+            <Router>
+                <div>
+                    <Header/>
+                    <Container>
+                        <Route path="/" exact component={Courses}/>
+                        <Route path="/course/:id" exact component={Course}/>
+                    </Container>
+                </div>
+            </Router>
+        );
+    }
+
 }
 
 export default App;
