a3bf3331645081e58fb70c33fb332cb6493b84e9
jakub <jakubstankowski@protonmail.com>
2021-03-30 19:36:29 +0200

    EL-29 add create new lesson. course buttons in Admin.js

--- WebUI/ClientApp/e-learning-ui/src/components/admin/Admin.js
+++ WebUI/ClientApp/e-learning-ui/src/components/admin/Admin.js
@@ -1,12 +1,30 @@
 import * as React from "react";
+import Typography from "@material-ui/core/Typography";
+import {Grid, Paper} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
 
 
 class Admin extends React.Component {
 
     render() {
         return (
-            <article>
-                admin!
+            <article style={{padding: 16}}>
+                <Typography variant="h5" component="h2">
+                    Admin mode
+                </Typography>
+                <Grid container alignItems="flex-start" spacing={2}>
+                    <Link to="/admin/course/create" style={{textDecoration: 'none'}}>
+                                <Button variant="contained" color="primary">
+                                    Create new course
+                                </Button>
+                    </Link>
+                    <Link to="/admin/course/create" style={{textDecoration: 'none'}}>
+                        <Button variant="contained" color="primary">
+                            Create new lesson
+                        </Button>
+                    </Link>
+                </Grid>
             </article>
         )
     }
