e3b8083463f4691c7f8d3de1500864a95cf1b006
jakub <jakubstankowski@protonmail.com>
2021-04-02 15:43:08 +0200

    Add create new button/lesson buttons in Header.js

--- WebUI/ClientApp/e-learning-ui/src/components/layout/Header.js
+++ WebUI/ClientApp/e-learning-ui/src/components/layout/Header.js
@@ -3,6 +3,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import {Link} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
 class Header extends React.Component {
     render() {
@@ -13,6 +14,16 @@ class Header extends React.Component {
                         <Typography variant="h6" color="inherit">
                             E-Learning
                         </Typography>
+                    </Link>
+                    <Link to="/admin/course/create">
+                        <Button>
+                            Create new course
+                        </Button>
+                    </Link>
+                    <Link to="/admin/course/lesson">
+                        <Button>
+                            Create new lesson
+                        </Button>
                     </Link>
                 </Toolbar>
             </AppBar>
