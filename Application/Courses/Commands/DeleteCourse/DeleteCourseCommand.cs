c82cf4a6887f335401df15d929b40313c624b549
jakub <jakubstankowski@protonmail.com>
2021-04-04 11:59:31 +0200

    Return list of courses after delete

--- Application/Courses/Commands/DeleteCourse/DeleteCourseCommand.cs
+++ Application/Courses/Commands/DeleteCourse/DeleteCourseCommand.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using E_Learning.Application.Common.Dto;
 using E_Learning.Application.Common.Exceptions;
 using E_Learning.Application.Common.Interfaces;
 using E_Learning.Domain.Entities;
@@ -8,23 +11,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace E_Learning.Application.Courses.Commands.DeleteCourse
 {
-    public class DeleteCourseCommand : IRequest<int>
+    public class DeleteCourseCommand : IRequest<IEnumerable<CourseDto>>
     {
         public int Id { get; set; }
 
     }
 
 
-    public class DeleteCourseHandler : IRequestHandler<DeleteCourseCommand, int>
+    public class DeleteCourseHandler : IRequestHandler<DeleteCourseCommand, IEnumerable<CourseDto>>
     {
         private readonly IContext _context;
+        private readonly IMapper _mapper;
 
-        public DeleteCourseHandler(IContext context)
+        public DeleteCourseHandler(IContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<int> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<CourseDto>> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
         {
             var course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == request.Id);
 
@@ -34,13 +39,15 @@ namespace E_Learning.Application.Courses.Commands.DeleteCourse
             }
 
             _context.Courses.Remove(course);
-           
+
             await _context.SaveChangesAsync();
 
+            var courses = await _context.Courses.ToListAsync();
 
-            return course.Id;
+            return _mapper.Map<IEnumerable<Course>, IEnumerable<CourseDto>>(courses);
+
         }
+     
 
-       
     }
 }
