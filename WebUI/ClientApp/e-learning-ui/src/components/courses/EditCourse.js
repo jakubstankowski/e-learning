9584123d6c308f80db0a666523199151c39c24da
jakub <jakubstankowski@protonmail.com>
2021-04-10 15:43:00 +0200

    EL-22 use hooks in EditCourse.js

--- WebUI/ClientApp/e-learning-ui/src/components/courses/EditCourse.js
+++ WebUI/ClientApp/e-learning-ui/src/components/courses/EditCourse.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, {Fragment, Component, useEffect} from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import Lessons from "../lessons/Lessons";
@@ -6,28 +6,20 @@ import {Field, Form} from "react-final-form";
 import {Button, Grid, Paper} from "@material-ui/core";
 import {TextField} from "final-form-material-ui";
 
-class EditCourse extends React.Component {
-    componentDidMount() {
-        this.props.getCourse(this.props.match.params.courseId);
-    }
 
-    static propTypes = {
-        course: PropTypes.object.isRequired,
-        lessons: PropTypes.array.isRequired,
-        getCourse: PropTypes.func.isRequired,
-        updateCourse: PropTypes.func.isRequired,
-        deleteLesson: PropTypes.func.isRequired,
-        updateLesson: PropTypes.func.isRequired
-    };
+function EditCourse({getCourse, updateCourse, deleteLesson, updateLesson, course, lessons, match, history, props}) {
+    useEffect(() => {
+        getCourse(match.params.courseId);
+    });
 
-    onSubmit = (course) => {
-        this.props.updateCourse(this.props.match.params.courseId, course)
+    const onSubmit = (course) => {
+        updateCourse(match.params.courseId, course)
             .then(() => {
-                this.props.history.push('/');
+                history.push('/');
             })
     };
 
-    validate = (values) => {
+    const validate = (values) => {
         const errors = {};
         if (!values.title) {
             errors.title = 'Required';
@@ -42,81 +34,85 @@ class EditCourse extends React.Component {
     };
 
 
-    render() {
-        const {id, title, description, price} = this.props.course;
-
-        return (
-            <section className='form-container'>
-                <Typography variant="h5" component="h2">
-                    Edit Course <strong>{title}</strong> ID: <strong>{id}</strong>
-                </Typography>
-                <Form
-                    initialValues={{
-                        id: id,
-                        title: title,
-                        description: description,
-                        price: price
-                    }}
-                    onSubmit={this.onSubmit}
-                    validate={this.validate}
-                    render={({handleSubmit, submitting}) => (
-                        <form onSubmit={handleSubmit} noValidate>
-                            <Paper style={{padding: 16}}>
-                                <Grid container alignItems="flex-start" spacing={2}>
-                                    <Grid item xs={12}>
-                                        <Field
-                                            fullWidth
-                                            required
-                                            name="title"
-                                            component={TextField}
-                                            type="text"
-                                            label="Title"
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Field
-                                            fullWidth
-                                            required
-                                            name="description"
-                                            component={TextField}
-                                            type="text"
-                                            label="Description"
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Field
-                                            fullWidth
-                                            required
-                                            name="price"
-                                            component={TextField}
-                                            type="number"
-                                            label="Price"
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Button type="submit"
-                                                className="form-button"
-                                                color="primary"
-                                                disabled={submitting}
-                                                variant="contained">
-                                            Save
-                                        </Button>
-                                    </Grid>
+    return (
+        <section className='form-container'>
+            <Typography variant="h5" component="h2">
+                Edit Course <strong>{course.title}</strong> ID: <strong>{course.id}</strong>
+            </Typography>
+            <Form
+                initialValues={{
+                    id: course.id,
+                    title: course.title,
+                    description: course.description,
+                    price: course.price
+                }}
+                onSubmit={onSubmit}
+                validate={validate}
+                render={({handleSubmit, submitting}) => (
+                    <form onSubmit={handleSubmit} noValidate>
+                        <Paper style={{padding: 16}}>
+                            <Grid container alignItems="flex-start" spacing={2}>
+                                <Grid item xs={12}>
+                                    <Field
+                                        fullWidth
+                                        required
+                                        name="title"
+                                        component={TextField}
+                                        type="text"
+                                        label="Title"
+                                    />
                                 </Grid>
-                            </Paper>
-                        </form>
-                    )}
-                />
-                <Lessons
-                    {...this.props}
-                    deleteLesson={this.props.deleteLesson}
-                    updateLesson={this.props.updateLesson}
-                    lessons={this.props.lessons}/>
-            </section>
-        )
-    }
+                                <Grid item xs={12}>
+                                    <Field
+                                        fullWidth
+                                        required
+                                        name="description"
+                                        component={TextField}
+                                        type="text"
+                                        label="Description"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Field
+                                        fullWidth
+                                        required
+                                        name="price"
+                                        component={TextField}
+                                        type="number"
+                                        label="Price"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button type="submit"
+                                            className="form-button"
+                                            color="primary"
+                                            disabled={submitting}
+                                            variant="contained">
+                                        Save
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </form>
+                )}
+            />
+          {/*  <Lessons
+                {...props}
+                deleteLesson={deleteLesson}
+                updateLesson={updateLesson}
+                lessons={lessons}/>*/}
+        </section>
+    )
 
 }
 
+EditCourse.propTypes = {
+    course: PropTypes.object.isRequired,
+    lessons: PropTypes.array.isRequired,
+    getCourse: PropTypes.func.isRequired,
+    updateCourse: PropTypes.func.isRequired,
+    deleteLesson: PropTypes.func.isRequired,
+    updateLesson: PropTypes.func.isRequired
+}
 
 export default EditCourse;
