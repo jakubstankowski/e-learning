8f6e5c609e3403673972222190f28f46793350cc
jakub <jakubstankowski@protonmail.com>
2021-04-14 16:13:25 +0200

    EL-23 add basic contexts files

---
+++ WebUI/ClientApp/e-learning-ui/src/context/courses/coursesContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const coursesContext = createContext();
+
+export default coursesContext;
---
+++ WebUI/ClientApp/e-learning-ui/src/context/lessons/lessonsContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const lessonsContext = createContext();
+
+export default lessonsContext;
