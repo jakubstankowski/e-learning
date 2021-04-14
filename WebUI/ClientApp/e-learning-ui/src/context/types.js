5daabcea34f7efbec1744d7ef720d376d047dff0
jakub <jakubstankowski@protonmail.com>
2021-04-14 16:16:53 +0200

    EL-23 add list of context types

---
+++ WebUI/ClientApp/e-learning-ui/src/context/types.js
@@ -0,0 +1,15 @@
+export const GET_COURSES = 'GET_COURSES';
+export const GET_COURSE = 'GET_COURSE';
+export const GET_LESSON = 'GET_LESSON';
+export const POST_COURSE = 'POST_COURSE';
+export const POST_LESSON = 'POST_LESSON';
+export const DELETE_COURSE = 'DELETE_COURSE';
+export const DELETE_LESSON = 'DELETE_LESSON';
+export const UPDATE_COURSE = 'UPDATE_COURSE';
+export const UPDATE_LESSON = 'UPDATE_LESSON';
+
+
+
+
+
+
