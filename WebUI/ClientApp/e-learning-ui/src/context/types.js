a8e83351504ee81d1282a59fd7e0d1d9cc0fc812
jakub <jakubstankowski@protonmail.com>
2021-04-17 12:48:25 +0200

    EL-17 add basic authorization components

--- WebUI/ClientApp/e-learning-ui/src/context/types.js
+++ WebUI/ClientApp/e-learning-ui/src/context/types.js
@@ -9,7 +9,13 @@ export const DELETE_LESSON = 'DELETE_LESSON';
 export const UPDATE_COURSE = 'UPDATE_COURSE';
 export const UPDATE_LESSON = 'UPDATE_LESSON';
 export const SET_LOADING = 'SET_LOADING';
-
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
 
 
 
