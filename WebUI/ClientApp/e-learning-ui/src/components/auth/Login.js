eedf4ac4928598d90f6707e070ae57b38d2472dd
jakub <jakubstankowski@protonmail.com>
2021-04-17 15:18:21 +0200

    EL-17 remove useStyles, and add Auth.css

--- WebUI/ClientApp/e-learning-ui/src/components/auth/Login.js
+++ WebUI/ClientApp/e-learning-ui/src/components/auth/Login.js
@@ -1,3 +1,4 @@
+import './Auth.css';
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -9,7 +10,6 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 function Copyright() {
@@ -25,40 +25,16 @@ function Copyright() {
     );
 }
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
 
 export default function Login() {
-    const classes = useStyles();
-
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                   avatar
-                </Avatar>
+            <div className="paper">
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-                <form className={classes.form} noValidate>
+                <form className="form" noValidate>
                     <TextField
                         variant="outlined"
                         margin="normal"
@@ -90,7 +66,7 @@ export default function Login() {
                         fullWidth
                         variant="contained"
                         color="primary"
-                        className={classes.submit}
+                        className="submit"
                     >
                         Sign In
                     </Button>
