ab59d5b6dd36e663f2b5680f7019aa7bb205b838
jakub <jakubstankowski@protonmail.com>
2021-04-18 12:36:47 +0200

    EL-17 add basic login forms in Login.js

--- WebUI/ClientApp/e-learning-ui/src/components/auth/Login.js
+++ WebUI/ClientApp/e-learning-ui/src/components/auth/Login.js
@@ -1,73 +1,92 @@
 import './Auth.css';
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import {TextField} from 'mui-rff';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import {Form} from "react-final-form";
 
 
 export default function Login() {
+
+    const onSubmit = (values) => {
+        console.log('values: ', values);
+        /* postCourse(values)
+             .then(() => {
+                 history.push('/');
+             })*/
+    };
+
+    const validate = (values) => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'Required';
+        }
+        if (!values.password) {
+            errors.password = 'Required';
+        }
+        return errors;
+    };
+
+
     return (
         <Container component="main" maxWidth="xs">
             <article className="paper">
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-                <form className="form" noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className="submit"
-                    >
-                        Sign In
-                    </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
+                <Form
+                    onSubmit={onSubmit}
+                    validate={validate}
+                    render={({handleSubmit, submitting}) => (
+                        <form onSubmit={handleSubmit} noValidate className="form">
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    )}/>
             </article>
         </Container>
     );
