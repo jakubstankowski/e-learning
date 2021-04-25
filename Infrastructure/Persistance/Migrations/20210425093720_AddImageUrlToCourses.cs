57fb3bae69f446c6047659b894efcd1bf52b7900
jakub <jakubstankowski@protonmail.com>
2021-04-25 11:41:38 +0200

    Add image url to courses

---
+++ Infrastructure/Persistance/Migrations/20210425093720_AddImageUrlToCourses.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistance.Migrations
+{
+    public partial class AddImageUrlToCourses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Courses");
+        }
+    }
+}
