fed169cc4386ae1f694a5669acee708a43e527d7
jakub <jakubstankowski@protonmail.com>
2021-05-01 15:56:41 +0200

    EL-39 add Dashboard.js component, and render in DashboardLayout.js

--- WebUI/ClientApp/e-learning-ui/src/App.js
+++ WebUI/ClientApp/e-learning-ui/src/App.js
@@ -12,11 +12,13 @@ import LessonsState from "./context/lessons/LessonsState";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import AuthState from "./context/auth/AuthState";
-import Dashboard from "./components/layout/Dashboard";
+import DashboardLayout from "./components/layout/DashboardLayout";
 import MainLayout from "./components/layout/MainLayout";
 import Lessons from "./components/lessons/Lessons";
 import Home from "./components/pages/Home";
 import Lesson from "./components/lessons/Lesson";
+import Dashboard from "./components/dashboard/Dashboard";
+
 
 function App() {
     return (
@@ -31,58 +33,11 @@ function App() {
                             <Login path="/login"/>
                             <Register path="/register"/>
                         </MainLayout>
-                        <Dashboard path="/dashboard">
-                            <CreateCourse path="/"/>
-                        </Dashboard>
+                        <DashboardLayout path="/dashboard">
+                            <Dashboard path="/"/>
+                            <CreateCourse path="/create/course"/>
+                        </DashboardLayout>
                     </Router>
-                    {/* <Router>
-                        <Header/>
-                            <Switch>
-
-                                <Route
-                                    path="/dashboard"
-                                    exact
-                                    component={Dashboard}
-                                />
-                                <Route
-                                    path="/course/:courseId"
-                                    component={Course}
-                                />
-                                <Route
-                                    exact
-                                    path="/admin"
-                                    component={Admin}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/admin/course/create"
-                                    component={CreateCourse}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/admin/course/:courseId/edit"
-                                    component={EditCourse}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/admin/lesson/:lessonId/edit"
-                                    component={EditLesson}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/admin/lesson/create"
-                                    component={CreateLesson}
-                                />
-                                <Route
-                                    exact
-                                    path='/register'
-                                    component={Register}/>
-                                <Route
-                                    exact
-                                    path='/login'
-                                    component={Login}/>
-                            </Switch>
-                    </Router>*/}
                 </LessonsState>
             </CoursesState>
         </AuthState>
---
+++ WebUI/ClientApp/e-learning-ui/src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import {Container} from "@material-ui/core";
+import React from "react";
+
+
+export default function Dashboard(){
+
+    return(
+        <Container>
+            dashboard
+        </Container>
+    )
+}
