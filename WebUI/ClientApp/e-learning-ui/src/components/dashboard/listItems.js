1e1141c7500f6be93cfe2cb96473b012500cbdcd
jakub <jakubstankowski@protonmail.com>
2021-05-02 12:55:18 +0200

    EL-39 add NavLink.js to  listItems.js

--- WebUI/ClientApp/e-learning-ui/src/components/dashboard/listItems.js
+++ WebUI/ClientApp/e-learning-ui/src/components/dashboard/listItems.js
@@ -5,51 +5,51 @@ import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import VideoCallOutlinedIcon from '@material-ui/icons/VideoCallOutlined';
-import {Link} from "@reach/router";
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
+import {NavLink} from "../routing/NavLink";
 
 export const mainListItems = (
     <div>
-        <Link to={'/dashboard'}>
+        <NavLink to={'/dashboard'}>
             <ListItem button>
                 <ListItemIcon>
                     <DashboardIcon/>
                 </ListItemIcon>
                 <ListItemText primary="Dashboard"/>
             </ListItem>
-        </Link>
-        <Link to={'/dashboard/course/create'}>
+        </NavLink>
+        <NavLink to={'/dashboard/course/create'}>
             <ListItem button>
                 <ListItemIcon>
                     <PostAddIcon/>
                 </ListItemIcon>
                 <ListItemText primary="Create Course"/>
             </ListItem>
-        </Link>
-        <Link to={'/dashboard/courses'}>
+        </NavLink>
+        <NavLink to={'/dashboard/courses'}>
             <ListItem button>
                 <ListItemIcon>
                     <ListAltOutlinedIcon/>
                 </ListItemIcon>
                 <ListItemText primary="Courses"/>
             </ListItem>
-        </Link>
-        <Link to={'/dashboard/lesson/create'}>
+        </NavLink>
+        <NavLink to={'/dashboard/lesson/create'}>
             <ListItem button>
                 <ListItemIcon>
                     <VideoCallOutlinedIcon/>
                 </ListItemIcon>
                 <ListItemText primary="Create Lesson"/>
             </ListItem>
-        </Link>
-        <Link to={'/dashboard/lesson/create'}>
+        </NavLink>
+        <NavLink to={'/dashboard/lesson/create'}>
             <ListItem button>
                 <ListItemIcon>
                     <VideocamOutlinedIcon/>
                 </ListItemIcon>
                 <ListItemText primary="Lessons"/>
             </ListItem>
-        </Link>
+        </NavLink>
     </div>
 );
