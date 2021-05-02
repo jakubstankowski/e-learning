3e0e8d2b78cc47b7f13d70a749d128fea8d694be
jakub <jakubstankowski@protonmail.com>
2021-05-02 13:05:31 +0200

    EL-39 add own general theme, use in whole app

--- WebUI/ClientApp/e-learning-ui/src/index.js
+++ WebUI/ClientApp/e-learning-ui/src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from "@material-ui/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#3f50b5',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+            <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
