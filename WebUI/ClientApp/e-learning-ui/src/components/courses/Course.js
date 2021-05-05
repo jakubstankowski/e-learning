a3e7a2eba7b6f6c2531559511ef270577e4f8aa7
jakub <jakubstankowski@protonmail.com>
2021-05-05 16:06:51 +0200

    EL-39 add isAuthenticated flag in Course.js, show edit and delete buttons, when is authenticated

--- WebUI/ClientApp/e-learning-ui/src/components/courses/Course.js
+++ WebUI/ClientApp/e-learning-ui/src/components/courses/Course.js
@@ -1,27 +1,38 @@
 import React, {useContext, useEffect} from "react";
 import Lessons from "../lessons/Lessons";
-import {useParams} from "@reach/router";
+import {Link, useParams} from "@reach/router";
 import CoursesContext from "../../context/courses/coursesContext";
 import Spinner from "../layout/Spinner";
 import {Container} from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
+import AuthContext from "../../context/auth/authContext";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
 
-export default function Course({match}) {
+export default function Course() {
     const coursesContext = useContext(CoursesContext);
 
-    const {getCourse, course, loading} = coursesContext;
+    const {getCourse, course, loading, deleteCourse} = coursesContext;
+
+    const authContext = useContext(AuthContext);
+    const {isAuthenticated} = authContext;
 
     const {courseId} = useParams();
 
     useEffect(() => {
-        getCourse(courseId);
-    }, []);
+        if (isAuthenticated) {
+            getCourse(courseId);
+        }
+    }, [isAuthenticated]);
 
     const useStyles = makeStyles((theme) => ({
         course: {
             marginTop: '1rem',
             textFloat: 'center'
+        },
+        lessons: {
+            marginTop: '2rem'
         }
     }));
 
@@ -36,7 +47,26 @@ export default function Course({match}) {
             <Typography component="h4" variant="h3">
                 {title}
             </Typography>
-            <Lessons/>
+            {
+                isAuthenticated &&
+                <article>
+                    <Link to={`/dashboard/courses/${courseId}/edit`}
+                          style={{textDecoration: 'none'}}
+                    >
+                        <Button color="primary" variant="contained">
+                            Edit
+                        </Button>
+                    </Link>
+                    <Button color="secondary"
+                            onClick={() => deleteCourse(courseId)}
+                            variant="contained">
+                        Delete
+                    </Button>
+                </article>
+            }
+            <Container className={classes.lessons}>
+                <Lessons/>
+            </Container>
         </Container>
     )
 
