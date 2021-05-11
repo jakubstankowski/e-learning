f93f1a54554298bd0b39b6653f623e97991173c3
jakub <jakubstankowski@protonmail.com>
2021-05-11 15:50:12 +0200

    Add ger user courses query

---
+++ Application/ApplicationUser/Queries/GetUserCoursesQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using E_Learning.Application.Common.Dto;
+using MediatR;
+
+namespace E_Learning.Application.ApplicationUser.Queries
+{
+    public class GetUserCoursesQuery : IRequest<CourseDto>
+    {
+        public int UserId { get; set; }
+
+        public GetUserCoursesQuery(int userId)
+        {
+            this.UserId = userId;
+        }
+
+        public class GetUserCoursesHandler : IRequestHandler<GetUserCoursesQuery, CourseDto>
+        {
+            public Task<CourseDto> Handle(GetUserCoursesQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
---
+++ Application/ApplicationUser/Queries/UserCoursesDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Learning.Application.Common.Dto;
+
+namespace E_Learning.Application.ApplicationUser.Queries
+{
+    public class UserCoursesDto
+    {
+        public IEnumerable<CourseDto> Courses { get; set; }
+
+    }
+}
