06b33aeca86e61f0598ac2c4330a2180e3f48931
jakub <jakubstankowski@protonmail.com>
2021-05-11 15:53:49 +0200

    Inject context, and mapper to GetuserCoursesQuery

--- Application/ApplicationUser/Queries/GetUserCoursesQuery.cs
+++ Application/ApplicationUser/Queries/GetUserCoursesQuery.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using E_Learning.Application.Common.Dto;
+using E_Learning.Application.Common.Interfaces;
 using MediatR;
 
 namespace E_Learning.Application.ApplicationUser.Queries
@@ -20,6 +22,15 @@ namespace E_Learning.Application.ApplicationUser.Queries
 
         public class GetUserCoursesHandler : IRequestHandler<GetUserCoursesQuery, CourseDto>
         {
+            private readonly IContext _context;
+            private readonly IMapper _mapper;
+
+            public GetUserCoursesHandler(IContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
             public Task<CourseDto> Handle(GetUserCoursesQuery request, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
