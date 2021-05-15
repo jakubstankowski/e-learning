af0695b294dd7941872c21c9e9ee1ec732321c51
jakub <jakubstankowski@protonmail.com>
2021-05-15 16:10:00 +0200

    EL-45 add getUserCourses methods in UserCourses.js

--- WebUI/ClientApp/e-learning-ui/src/components/user/UserCourses.js
+++ WebUI/ClientApp/e-learning-ui/src/components/user/UserCourses.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import UserContext from '../../context/user/userContext';
+import AuthContext from "../../context/auth/authContext";
 
+export default function UserCourses() {
+    const userContext = useContext(UserContext);
+    const {getUserCourses} = userContext;
 
-export default function UserCourses(){
+    const authContext = useContext(AuthContext);
+    const {isAuthenticated, userId} = authContext;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            getUserCourses(userId);
+        }
+
+    }, [isAuthenticated]);
+
     return (
         <div>
             user courses
