b7373388c4e24dbc0437c8aafa6dfb2e97134d5b
jakub <jakubstankowski@protonmail.com>
2021-05-17 22:08:23 +0200

    Remove ApplicationUser and use only IdentityUser

---
+++ Domain/Entities/TestUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace E_Learning.Domain.Entities
+{
+    public class TestUser: IdentityUser
+    {
+    }
+}
---
+++ Infrastructure/Persistance/Migrations/20210517180959_RemoveApplicationUserFromContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistance.Migrations
+{
+    public partial class RemoveApplicationUserFromContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_AspNetUsers_ApplicationUserId",
+                table: "UserCourses");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "UserCourses",
+                newName: "IdentityUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserCourses_ApplicationUserId",
+                table: "UserCourses",
+                newName: "IX_UserCourses_IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_AspNetUsers_IdentityUserId",
+                table: "UserCourses",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_AspNetUsers_IdentityUserId",
+                table: "UserCourses");
+
+            migrationBuilder.RenameColumn(
+                name: "IdentityUserId",
+                table: "UserCourses",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserCourses_IdentityUserId",
+                table: "UserCourses",
+                newName: "IX_UserCourses_ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_AspNetUsers_ApplicationUserId",
+                table: "UserCourses",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
