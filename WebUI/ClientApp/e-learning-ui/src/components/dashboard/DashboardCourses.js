9f3950f59d8e9709fe9af57e34a774e4c62c364e
jakub <jakubstankowski@protonmail.com>
2021-05-19 18:51:56 +0200

    Add DashboardCourses.js component

---
+++ WebUI/ClientApp/e-learning-ui/src/components/dashboard/DashboardCourses.js
@@ -0,0 +1,43 @@
+import Container from "@material-ui/core/Container";
+import React, {useContext, useEffect} from "react";
+import CoursesContext from "../../context/courses/coursesContext";
+import AuthContext from "../../context/auth/authContext";
+import Spinner from "../layout/Spinner";
+import CourseItem from "../courses/CourseItem";
+import Grid from "@material-ui/core/Grid";
+
+export default function DashboardCourses() {
+
+    const coursesContext = useContext(CoursesContext);
+
+    const {getCourses, courses, loading} = coursesContext;
+
+    const authContext = useContext(AuthContext);
+
+    const {isAuthenticated} = authContext;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            getCourses();
+        }
+
+    }, [isAuthenticated]);
+
+    if (loading) return <Spinner/>
+
+    return (
+        <Container>
+            Dashboard Courses
+            <Grid container spacing={4}>
+                {
+                    courses.map((course) =>
+                        <CourseItem
+                            course={course}
+                            key={course.id}/>
+                    )
+                }
+            </Grid>
+        </Container>
+    )
+
+}
