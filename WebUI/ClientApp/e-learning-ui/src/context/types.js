fdd4f295d3898968d13b4c29f1ae854f91aeaa1d
jakub <jakubstankowski@protonmail.com>
2021-05-24 20:00:30 +0200

    EL-44 add context cart types

--- WebUI/ClientApp/e-learning-ui/src/context/types.js
+++ WebUI/ClientApp/e-learning-ui/src/context/types.js
@@ -17,6 +17,9 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+export const POST_CART = 'POST_CART';
+export const DELETE_CART = 'DELETE_CART';
+export const GET_CART = 'GET_CART';
 
 
 
