10d5935ecc74b5d35c1e57335699d1450129c9fa
jakub <jakubstankowski@protonmail.com>
2021-05-24 19:56:43 +0200

    EL-44 change basic layout styles in HomeLayout.js

--- WebUI/ClientApp/e-learning-ui/src/components/layout/HomeLayout.js
+++ WebUI/ClientApp/e-learning-ui/src/components/layout/HomeLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import {Container} from "@material-ui/core";
 import Footer from "./Footer";
+import {makeStyles} from "@material-ui/core/styles";
 
 
 const sections = [
@@ -17,12 +18,20 @@ const sections = [
     {title: 'Travel', url: '#'},
 ];
 
+const useStyles = makeStyles((theme) => ({
+    homeContainer: {
+        minHeight: '90vh'
+    },
+}));
+
 
 export default function HomeLayout({children}) {
+    const classes = useStyles();
+
     return (
         <main>
             <Header title="stanlearn" sections={sections}/>
-            <Container>
+            <Container className={classes.homeContainer}>
                 {children}
             </Container>
             <Footer title="Footer" description="Something here to give the footer a purpose!"/>
