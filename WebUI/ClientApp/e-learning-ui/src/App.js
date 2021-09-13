590b1d57fb46d530cdd39403c56f3669ba6a2e48
jakub <jakubstankowski@protonmail.com>
2021-09-13 15:40:23 +0200

    Add basket state in app.js

--- WebUI/ClientApp/e-learning-ui/src/App.js
+++ WebUI/ClientApp/e-learning-ui/src/App.js
@@ -8,6 +8,7 @@ import EditCourse from "./components/courses/EditCourse";
 import EditLesson from "./components/lessons/EditLesson";
 import CoursesState from "./context/courses/CoursesState";
 import LessonsState from "./context/lessons/LessonsState";
+import BasketState from './context/basket/BasketState';
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import AuthState from "./context/auth/AuthState";
@@ -27,32 +28,34 @@ function App() {
     return (
         <AuthState>
             <CoursesState>
-                <LessonsState>
-                    <Router>
-                        <HomeLayout path="/">
-                            <Home path="/"/>
-                            <Course path="/course/:courseId"/>
-                            <Lesson path="/course/:courseId/lesson/:lessonId"/>
-                            <Login path="/login"/>
-                            <Register path="/register"/>
-                            <Basket path="/basket"/>
-                        </HomeLayout>
-                        <DashboardLayout path="/dashboard">
-                            <Dashboard path="/"/>
-                            <CreateCourse path="/course/create"/>
-                            <EditCourse path="/courses/:courseId/edit"/>
-                            <DashboardCourses path="/courses"/>
-                            <Course path="/course/:courseId"/>
-                            <Lesson path="/course/:courseId/lesson/:lessonId"/>
-                            <CreateLesson path="/course/:courseId/lesson/create"/>
-                            <EditLesson path="/course/:courseId/lesson/:lessonId/edit"/>
-                        </DashboardLayout>
-                        <UserLayout path="/user">
-                            <UserCourses path="/my-courses"/>
-                            <UserProfile path="/profile"/>
-                        </UserLayout>
-                    </Router>
-                </LessonsState>
+                <BasketState>
+                    <LessonsState>
+                        <Router>
+                            <HomeLayout path="/">
+                                <Home path="/"/>
+                                <Course path="/course/:courseId"/>
+                                <Lesson path="/course/:courseId/lesson/:lessonId"/>
+                                <Login path="/login"/>
+                                <Register path="/register"/>
+                                <Basket path="/basket"/>
+                            </HomeLayout>
+                            <DashboardLayout path="/dashboard">
+                                <Dashboard path="/"/>
+                                <CreateCourse path="/course/create"/>
+                                <EditCourse path="/courses/:courseId/edit"/>
+                                <DashboardCourses path="/courses"/>
+                                <Course path="/course/:courseId"/>
+                                <Lesson path="/course/:courseId/lesson/:lessonId"/>
+                                <CreateLesson path="/course/:courseId/lesson/create"/>
+                                <EditLesson path="/course/:courseId/lesson/:lessonId/edit"/>
+                            </DashboardLayout>
+                            <UserLayout path="/user">
+                                <UserCourses path="/my-courses"/>
+                                <UserProfile path="/profile"/>
+                            </UserLayout>
+                        </Router>
+                    </LessonsState>
+                </BasketState>
             </CoursesState>
         </AuthState>
     );
