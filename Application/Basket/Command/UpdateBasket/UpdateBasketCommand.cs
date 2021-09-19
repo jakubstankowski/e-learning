516ca3f3775df0380ceb33644cc73a623ff2fb61
jakub <jakubstankowski@protonmail.com>
2021-09-19 09:47:36 +0200

    EL-44 check if basket is available

--- Application/Basket/Command/UpdateBasket/UpdateBasketCommand.cs
+++ Application/Basket/Command/UpdateBasket/UpdateBasketCommand.cs
@@ -5,9 +5,13 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using E_Learning.Application.Basket.Queries.GetBasket;
+using E_Learning.Application.Common.Exceptions;
+using E_Learning.Application.Common.Interfaces;
 using E_Learning.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
 namespace E_Learning.Application.Basket.Command.UpdateBasket
@@ -21,19 +25,34 @@ namespace E_Learning.Application.Basket.Command.UpdateBasket
     public class UpdateBasketHandler : IRequestHandler<UpdateBasketCommand, CustomerBasket>
     {
         private readonly IDatabase _database;
+        private readonly IContext _context;
+        public IMapper _mapper { get; }
 
-        public UpdateBasketHandler(IConnectionMultiplexer redis)
+        public UpdateBasketHandler(IConnectionMultiplexer redis, IContext context)
         {
             _database = redis.GetDatabase();
+            _context = context;
         }
 
         public async Task<CustomerBasket> Handle(UpdateBasketCommand request, CancellationToken cancellationToken)
         {
+
             var basket = new CustomerBasket
             {
                 Id = request.Id,
                 Items = request.Items
             };
+
+            foreach (var item in basket.Items)
+            {
+                var course = await _context
+                         .Courses.FirstOrDefaultAsync(c => c.Id == item.Id);
+
+                if (course == null)
+                {
+                    throw new NotFoundException(nameof(Course), item.Id);
+                }
+            }
 
             var created = await _database.StringSetAsync(request.Id,
                 JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
