23cfc92eddeead9b4fe2c65833d1a3e83ee61fd5
jakub <jakubstankowski@protonmail.com>
2021-09-27 15:23:46 +0200

    EL-44 refactor CourseTitle to Title, add Price in BaskeItem entity

--- Domain/Entities/BasketItem.cs
+++ Domain/Entities/BasketItem.cs
@@ -10,7 +10,10 @@ namespace E_Learning.Domain.Entities
     {
         public int Id { get; set; }
 
-        public string CourseTitle { get; set; }
+        public string Title { get; set; }
+
+        public string Price { get; set; }
+
 
     }
 }
