dd134e0151bd355efc70015da69069ffc73ceb73
jakub <jakubstankowski@protonmail.com>
2021-09-28 15:06:51 +0200

    EL-44 add get basket by id in Basket.js

---
+++ WebUI/ClientApp/e-learning-ui/src/components/basket/BasketItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BasketItem() {
+
+    return (
+        <div>
+            basket item
+        </div>
+    )
+}
--- WebUI/ClientApp/e-learning-ui/src/components/pages/Basket.js
+++ WebUI/ClientApp/e-learning-ui/src/components/pages/Basket.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import {useContext, useEffect} from "react";
+import BasketContext from '../../context/basket/basketContext';
 
 export default function Cart() {
+    const basketContext = useContext(BasketContext);
+
+    const {getBasket, basket} = basketContext;
+
+    useEffect(() => {
+        const basketId = localStorage.getItem('basket_id');
+
+        if (basketId) {
+            getBasket(basketId)
+                .then((res) => {
+                    console.log('get basket res: ', res);
+                })
+        }
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <div>
