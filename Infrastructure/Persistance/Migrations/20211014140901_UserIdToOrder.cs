5c1c1e807c274ec56e4f41bbb349751e07da4ac4
jakub <jakubstankowski@protonmail.com>
2021-10-14 16:10:00 +0200

    EL-48 add userId to order

---
+++ Infrastructure/Persistance/Migrations/20211014140901_UserIdToOrder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistance.Migrations
+{
+    public partial class UserIdToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "Orders");
+        }
+    }
+}
