bdacdbb7e977c497fee890e9481435be4454bb27
jakub <jakubstankowski@protonmail.com>
2021-10-14 17:03:04 +0200

    EL-48  implement get order by user query

---
+++ Application/Orders/Queries/GetOrders/GetOrderByUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace E_Learning.Application.Orders.Queries.GetOrders
+{
+    public class GetOrderByUserQuery IRequest<IEnumerable<CourseDto>>
+    {
+
+    }
+}
