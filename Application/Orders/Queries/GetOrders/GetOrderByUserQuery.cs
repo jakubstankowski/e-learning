3e60ae1c11dcfd7bdda90971fba1919e73e73d58
jakub <jakubstankowski@protonmail.com>
2021-10-14 17:13:41 +0200

    EL-48 implement get order by user query functionality

--- Application/Orders/Queries/GetOrders/GetOrderByUserQuery.cs
+++ Application/Orders/Queries/GetOrders/GetOrderByUserQuery.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using E_Learning.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Learning.Application.Orders.Queries.GetOrders
 {
-    public class GetOrderByUserQuery IRequest<IEnumerable<CourseDto>>
+    public class GetOrderByUserQuery : IRequest<IEnumerable<OrderDto>>
     {
 
     }
+
+    public class GetOrderByUserQueryHandler : IRequestHandler<GetOrderByUserQuery, IEnumerable<OrderDto>>
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+
+        public GetOrderByUserQueryHandler(IIdentityService identityService, IContext context, IMapper mapper)
+        {
+            _identityService = identityService;
+            _context = context;
+            _mapper = mapper;
+        }
+        public Task<IEnumerable<OrderDto>> Handle(GetOrderByUserQuery request, CancellationToken cancellationToken)
+        {
+            string userId = _identityService.GetUserId();
+
+            var orders = _context.Orders.Where(o => o.BuyerId == userId);
+
+            throw new NotImplementedException();
+        }
+    }
+
 }
--- Application/Orders/Queries/GetOrders/OrderDto.cs
+++ Application/Orders/Queries/GetOrders/OrderDto.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using E_Learning.Domain.Entities.OrderAggregate;
 
 namespace E_Learning.Application.Orders.Queries.GetOrders
 {
     public class OrderDto
     {
-        public string BasketId { get; set; }
+        public int Id { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
     }
 }
