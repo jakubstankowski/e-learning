832fea6788cdf2ea6ffd09d37bf6897f432fadad
jakub <jakubstankowski@protonmail.com>
2021-10-15 14:52:28 +0200

    EL-50 add checkout button in basket view

--- WebUI/ClientApp/e-learning-ui/src/pages/basket/Basket.js
+++ WebUI/ClientApp/e-learning-ui/src/pages/basket/Basket.js
@@ -1,18 +1,14 @@
 import React from "react";
 import {useContext, useEffect} from "react";
 import BasketContext from '../../context/basket/basketContext';
-import OrderContext from '../../context/order/orderContext';
 import BasketItem from "../../components/basket/BasketItem";
 import AuthContext from "../../context/auth/authContext";
 import Button from "@material-ui/core/Button";
+import {Link} from "@reach/router";
 
 export default function Cart() {
     const basketContext = useContext(BasketContext);
     const {getBasket, basket} = basketContext;
-
-    const orderContext = useContext(OrderContext);
-    const {postOrder} = orderContext;
-
 
     const authContext = useContext(AuthContext);
     const {isAuthenticated} = authContext;
@@ -45,11 +41,12 @@ export default function Cart() {
             </h4>
             {
                 isAuthenticated && basket.items.length > 0 &&
-                <Button color="secondary"
-                        onClick={() => postOrder(basket.id)}
-                        variant="contained">
-                    Finish Order
-                </Button>
+                <Link to="/checkout">
+                    <Button color="secondary"
+                            variant="contained">
+                        Proceed to checkout
+                    </Button>
+                </Link>
             }
         </div>
     )
