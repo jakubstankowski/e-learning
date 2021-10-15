331e7f4c01b8e8a3b93babbfbbcb9bea31701672
jakub <jakubstankowski@protonmail.com>
2021-10-15 15:24:09 +0200

    EL-50 implement basic checkout payment form

--- WebUI/ClientApp/e-learning-ui/src/pages/checkout/Checkout.js
+++ WebUI/ClientApp/e-learning-ui/src/pages/checkout/Checkout.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, {useContext} from "react";
+import BasketContext from "../../context/basket/basketContext";
+import Button from "@material-ui/core/Button";
+import {Form} from "react-final-form";
+import {TextField} from "mui-rff";
+import Grid from "@material-ui/core/Grid";
 
 export default function Checkout() {
+    const basketContext = useContext(BasketContext);
+    const {basket} = basketContext;
+
+    const onSubmit = (values) => {
+        console.log('values: ', values);
+    };
 
     return (
         <>
-            checkout page!
+            <h4>
+                total payment: {basket.subTotal}
+            </h4>
+            <Grid container spacing={4}>
+                <Grid item lg={4} xs={12}>
+                    <Form
+                        onSubmit={onSubmit}
+                        render={({handleSubmit, submitting}) => (
+                            <form onSubmit={handleSubmit}
+                                  className="form">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    fullWidth
+                                    id="name-on-card"
+                                    label="Name On Card"
+                                    name="nameOnCard"
+                                    autoFocus
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    fullWidth
+                                    type="number"
+                                    id="card-number"
+                                    label="Curd Number"
+                                    name="cardNumber"
+                                    autoFocus
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    fullWidth
+                                    id="card-expired"
+                                    label="Card Expired"
+                                    name="cardExpired"
+                                    autoFocus
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    fullWidth
+                                    id="card-cvc"
+                                    label="Card CVC"
+                                    name="cardCvc"
+                                    autoFocus
+                                />
+                                <Button color="secondary"
+                                        type="submit"
+                                        variant="contained">
+                                    Submit order
+                                </Button>
+                            </form>
+                        )}/>
+                </Grid>
+            </Grid>
         </>
     )
 }
