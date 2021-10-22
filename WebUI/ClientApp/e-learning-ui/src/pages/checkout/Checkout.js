cc94ad64cffac1901964e0de6235aa84faa8a5cc
jakub <jakubstankowski@protonmail.com>
2021-10-22 15:52:30 +0200

    EL-10 implement totaly new solution with payment form

--- WebUI/ClientApp/e-learning-ui/src/pages/checkout/Checkout.js
+++ WebUI/ClientApp/e-learning-ui/src/pages/checkout/Checkout.js
@@ -1,7 +1,30 @@
 import React, {useContext, useState} from "react";
 import BasketContext from "../../context/basket/basketContext";
 import PaymentsContext from "../../context/payments/paymentsContext";
-import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
+import {
+    useStripe, useElements,
+    CardExpiryElement,
+    CardNumberElement,
+    CardCvcElement
+} from '@stripe/react-stripe-js';
+import Button from "@material-ui/core/Button";
+
+
+const ELEMENT_OPTIONS = {
+    style: {
+        base: {
+            fontSize: '18px',
+            color: '#424770',
+            letterSpacing: '0.025em',
+            '::placeholder': {
+                color: '#aab7c4',
+            },
+        },
+        invalid: {
+            color: '#9e2146',
+        },
+    },
+};
 
 export default function Checkout() {
     const stripe = useStripe()
@@ -13,47 +36,58 @@ export default function Checkout() {
     const paymentContext = useContext(PaymentsContext);
     const {createPaymentIntent} = paymentContext;
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    const onSubmit = async (values) => {
+        values.preventDefault();
+        /*
+                 if (!stripe || !elements) {
+                     return;
+                 }
 
-        if (!stripe || !elements) {
-            return;
-        }
+                 await createPaymentIntent(basket.id);
 
-        await createPaymentIntent(basket.id);
+                 const {paymentIntent, error} = await stripe.confirmCardPayment(
+                     basket.clientSecret,
+                     {
+                         payment_method: {
+                             card: elements.getElement(CardElement),
+                             billing_details: {
+                                 name: 'Jenny Rosen',
+                             },
+                         },
+                     },
+                 );
 
-        const {paymentIntent, error} = await stripe.confirmCardPayment(
-            basket.clientSecret,
-            {
-                payment_method: {
-                    card: elements.getElement(CardElement),
-                    billing_details: {
-                        name: 'Jenny Rosen',
-                    },
-                },
-            },
-        );
+                 if (error) {
+                     alert('error!');
+                 }
 
-        if (error) {
-            alert('error!');
-        }
-
-        if (paymentIntent) {
-            alert('payment intent!');
-        }
+                 if (paymentIntent) {
+                     alert('payment intent!');
+                 }*/
 
     };
 
+
     return (
-        <>
-            <form onSubmit={onSubmit}>
-                <fieldset className="FormGroup">
-                    <div className="FormRow">
-                        <CardElement/>
-                    </div>
-                </fieldset>
-                <button>Pay</button>
-            </form>
-        </>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="cardNumber">Card Number</label>
+            <CardNumberElement
+                id="cardNumber"
+                options={ELEMENT_OPTIONS}
+            />
+            <label htmlFor="expiry">Card Expiration</label>
+            <CardExpiryElement
+                id="expiry"
+                options={ELEMENT_OPTIONS}
+            />
+            <label htmlFor="cvc">CVC</label>
+            <CardCvcElement
+                id="cvc"
+                options={ELEMENT_OPTIONS}
+            />
+            <Button type="submit" disabled={!stripe}>
+                Pay
+            </Button>
+        </form>
     )
 }
