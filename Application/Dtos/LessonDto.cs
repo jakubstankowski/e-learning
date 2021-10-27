2e9c4b2b8e1eaa7e8c4e7ecfbd9cd6bc88226af8
jakub <jakubstankowski@protonmail.com>
2021-10-27 15:23:45 +0200

    EL-51 add lesson service, and create lesson implementation

---
+++ Application/Dtos/LessonDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Application.Lessons.Queries.GetLessons
+{
+    public class LessonDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public int CourseId { get; set; }
+
+        public int? NextLessonId { get; set; }
+
+        public int? PreviousLessonId { get; set; }
+    }
+}
