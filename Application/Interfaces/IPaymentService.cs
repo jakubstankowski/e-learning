890255de2ea5e7526f57bb9146ffa03d8b955b88
jakub <jakubstankowski@protonmail.com>
2021-10-29 15:55:49 +0200

    EL-51 register payment service

--- Application/Interfaces/IPaymentService.cs
+++ Application/Interfaces/IPaymentService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using E_Learning.Domain.Entities;
 
 namespace E_Learning.Application.Interfaces
 {
     public interface IPaymentService
     {
+        public Task<CustomerBasket> CreateOrUpdatePaymentIntent();
     }
 }
--- Application/Services/PaymentService.cs
+++ Application/Services/PaymentService.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using E_Learning.Application.Interfaces;
+using E_Learning.Domain.Entities;
 
 namespace E_Learning.Application.Services
 {
     public class PaymentService : IPaymentService
     {
+        public Task<CustomerBasket> CreateOrUpdatePaymentIntent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
