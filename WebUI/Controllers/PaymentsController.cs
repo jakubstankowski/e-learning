74932d4018dfe717ccd902e74a28db65280cc807
jakub <jakubstankowski@protonmail.com>
2021-11-01 16:47:13 +0100

    EL-10 add basic stripe webhook endpoint

--- WebUI/Controllers/PaymentsController.cs
+++ WebUI/Controllers/PaymentsController.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using E_Learning.Application.Interfaces;
 using E_Learning.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace E_Learning.Controllers
 {
@@ -11,6 +13,7 @@ namespace E_Learning.Controllers
     public class PaymentsController : ControllerBase
     {
         private readonly IPaymentService _paymentService;
+        const string ENDPOINT_SECRET = "whsec_jb8roI0tP4UREYhL8DsrzXNOaipeXr5W";
 
         public PaymentsController(IPaymentService paymentService)
         {
@@ -25,5 +28,36 @@ namespace E_Learning.Controllers
 
             return Ok(basket);
         }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], ENDPOINT_SECRET);
+
+                // Handle the event
+                if (stripeEvent.Type == Events.PaymentIntentPaymentFailed)
+                {
+                }
+                else if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                }
+                // ... handle other event types
+                else
+                {
+
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
+
