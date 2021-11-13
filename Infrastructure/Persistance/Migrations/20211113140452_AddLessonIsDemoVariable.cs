a57f4a4a63c287b1e3f11484c84291a855a9e939
jakub <jakubstankowski@protonmail.com>
2021-11-13 15:07:17 +0100

    EL-53 add lesson demo migration

---
+++ Infrastructure/Persistance/Migrations/20211113140452_AddLessonIsDemoVariable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistance.Migrations
+{
+    public partial class AddLessonIsDemoVariable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDemo",
+                table: "Lessons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDemo",
+                table: "Lessons");
+        }
+    }
+}
